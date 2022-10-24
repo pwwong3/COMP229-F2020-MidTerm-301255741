c4429c2aadd805d7128a346d8918a6feb03f0ac2
Wong Pak Wah <pwwong3@users.noreply.github.com>
2022-10-24 01:38:33 -0400

    Fix the connection string of MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://pak:7tPio0mL66u88wjA@mongodbserver.gssemhf.mongodb.net/books229?retryWrites=true&w=majority/"
+  "URI": "mongodb+srv://pak:7tPio0mL66u88wjA@mongodbserver.gssemhf.mongodb.net/books229?retryWrites=true&w=majority"
 };
