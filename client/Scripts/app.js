2713ab1f85e1167ef90f7a3b271045f134f16133
Wong Pak Wah <pwwong3@users.noreply.github.com>
2022-10-24 12:45:43 -0400

    Revert "Upgrade the navbar to compatible for bootstrap 4 and fix a bug in app.js"
    This reverts commit 88c343f882e33deb48de4296f70e993f21427d6a.

--- client/Scripts/app.js
+++ client/Scripts/app.js
@@ -23,7 +23,7 @@ $.fn.pageMe = function(opts){
         pager = $(settings.pagerSelector);
     }
 
-    var numItems = children.length;
+    var numItems = children.size();
     var numPages = Math.ceil(numItems/perPage);
 
     pager.data("curr",0);
